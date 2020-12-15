6785b67f3c28041764f59264eb9999932fa0b422
justinfernald <jfernald@my.gcu.edu>
2020-12-15 09:20:22 -0700

    setting up prefabs

---
+++ Assets/Scripts/TileGrid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGrid : MonoBehaviour
+{
+    public int width = 20;
+    public int height = 15;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
